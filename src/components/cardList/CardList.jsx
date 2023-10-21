372b2f7be2bc7da3bef4fadb583ebe1a3e1105db
MustafaYavas <mustafayavas40@gmail.com>
2023-10-21 13:01:46 +0300

    Fix typo

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -26,7 +26,7 @@ const CardList = async ({ page, cat }) => {
   const POST_PER_PAGE = 2;
 
   const hasPrev = POST_PER_PAGE * (page - 1) > 0;
-  const hasNExt = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
   return (
     <div className={styles.container}>
